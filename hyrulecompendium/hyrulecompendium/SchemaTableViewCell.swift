3f4359081c2ec26602cc0a3d90c5464378a21eb5
audreydkim <audreydkim@icloud.com>
2024-06-19 22:56:43 -0700

    add table view cells for compendium schema buttons

---
+++ hyrulecompendium/hyrulecompendium/SchemaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SchemaTableViewCell.swift
+//  hyrulecompendium
+//
+//  Created by Audrey Kim on 6/19/24.
+//
+
+import UIKit
+
+class SchemaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
