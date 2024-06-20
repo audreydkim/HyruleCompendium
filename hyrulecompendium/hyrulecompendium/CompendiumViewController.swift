27845317d9cedd71bff78c10652814a7d644359c
audreydkim <audreydkim@icloud.com>
2024-06-19 22:59:21 -0700

    j

--- hyrulecompendium/hyrulecompendium/CompendiumViewController.swift
+++ hyrulecompendium/hyrulecompendium/CompendiumViewController.swift
@@ -7,17 +7,33 @@
 
 import UIKit
 
-class CompendiumViewController: UIViewController {
-
-    @IBOutlet weak var monstersButton: UIButton!
+class CompendiumViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let schemas: [String] = ["Monsters", "Creatures", "Equipment", "Materials", "Treasure"]
+//    let images: [String]
+    
+    @IBOutlet weak var compTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let nib = UINib(nibName: "SchemaTableViewCell", bundle: nil)
+        compTableView.register(nib, forCellReuseIdentifier: SchemaTableViewCell.identifier)
         // Do any additional setup after loading the view.
         loadButtonDetails()
+        compTableView.delegate = self
+        compTableView.dataSource = self
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return schemas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SchemaTableViewCell.identifier, for: indexPath) as! SchemaTableViewCell
+        cell.button.setTitle(schemas[indexPath.row], for: .normal)
+        return cell
+    }
+    
     private func loadButtonDetails() {
         //monstersButton.setTitle("Monsters", for: .normal)
     }
--- hyrulecompendium/hyrulecompendium/SchemaTableViewCell.swift
+++ hyrulecompendium/hyrulecompendium/SchemaTableViewCell.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class SchemaTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var button: UIButton!
+    static let identifier = "SchemaTableViewCell"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
